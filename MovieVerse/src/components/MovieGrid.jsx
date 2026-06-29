e994573718df44fbde8ef1f4ec16d07f754d318c
Anisha Ranjan <anisharanjanaur2007@gmail.com>
2026-06-29 20:12:58 +0530

    Add TMDB API service setup

--- MovieVerse/src/components/MovieGrid.jsx
+++ MovieVerse/src/components/MovieGrid.jsx
@@ -1,5 +1,44 @@
 import MovieCard from "./MovieCard";
 
+const movies = [
+  {
+    title: "Interstellar",
+    year: "2014",
+    duration: "2h 49m",
+    genre: "Sci-Fi",
+    rating: "9.2",
+    image:
+      "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+  },
+  {
+    title: "Oppenheimer",
+    year: "2023",
+    duration: "3h",
+    genre: "Drama",
+    rating: "8.8",
+    image:
+      "https://image.tmdb.org/t/p/w500/ptpr0kGAckfQkJeJIt8st5dglvd.jpg",
+  },
+  {
+    title: "The Batman",
+    year: "2022",
+    duration: "2h 56m",
+    genre: "Action",
+    rating: "8.3",
+    image:
+      "https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg",
+  },
+  {
+    title: "Dune",
+    year: "2021",
+    duration: "2h 35m",
+    genre: "Adventure",
+    rating: "8.4",
+    image:
+      "https://image.tmdb.org/t/p/w500/d5NXSklXo0qyIYkgV94XAgMIckC.jpg",
+  },
+];
+
 const MovieGrid = () => {
   return (
     <section className="movies-section">
@@ -13,12 +52,9 @@ const MovieGrid = () => {
       </div>
 
       <div className="movie-grid">
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
-        <MovieCard />
+        {movies.map((movie, index) => (
+          <MovieCard key={index} movie={movie} />
+        ))}
       </div>
     </section>
   );
---
+++ MovieVerse/src/services/api.js
@@ -0,0 +1,15 @@
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getTrendingMovies = async () => {
+  const response = await fetch(
+    `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch trending movies");
+  }
+
+  return response.json();
+};
