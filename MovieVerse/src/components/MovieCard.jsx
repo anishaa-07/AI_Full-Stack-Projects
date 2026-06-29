36b72572c89f4e528f2664ff35db0ddab493b98b
Anisha Ranjan <anisharanjanaur2007@gmail.com>
2026-06-29 20:15:53 +0530

    Display dynamic TMDB movie data

--- MovieVerse/src/components/MovieCard.jsx
+++ MovieVerse/src/components/MovieCard.jsx
@@ -1,37 +1,30 @@
 import { FaStar } from "react-icons/fa";
 
 const MovieCard = ({ movie }) => {
+  const poster = movie.poster_path
+    ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    : "https://placehold.co/500x750?text=No+Image";
+
   return (
     <div className="movie-card">
+      <div className="movie-poster">
+        <img src={poster} alt={movie.title} />
 
-      <div className="movie-image">
-
-        <img
-          src={movie.image}
-          alt={movie.title}
-        />
-
-        <span className="rating">
+        <div className="movie-rating">
           <FaStar />
-          {movie.rating}
-        </span>
-
+          <span>{movie.vote_average.toFixed(1)}</span>
+        </div>
       </div>
 
-      <div className="movie-content">
-
-        <span className="genre">
-          {movie.genre}
-        </span>
+      <div className="movie-info">
+        <p className="movie-category">
+          {movie.original_language.toUpperCase()}
+        </p>
 
         <h3>{movie.title}</h3>
 
-        <p>
-          {movie.year} • {movie.duration}
-        </p>
-
+        <span>{movie.release_date}</span>
       </div>
-
     </div>
   );
 };
